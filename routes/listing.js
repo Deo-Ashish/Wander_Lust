ea879bd65c2515b92aa5948d75f44e53ac48bf42
Deo-Ashish <deoashish942@gmail.com>
2026-06-05 22:41:56 +0530

    implemented authorization

--- routes/listing.js
+++ routes/listing.js
@@ -1,19 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
-const { isLoggedIn } = require("../middleware.js");
-
-const validateListing = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body);
-  if (error) {
-    throw new ExpressError(400, errorMsg);
-  } else {
-    next();
-  }
-};
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 router.get(
   "/",
@@ -25,7 +14,6 @@ router.get(
 
 //new route
 router.get("/new", isLoggedIn, (req, res) => {
-  console.log(req.user);
   res.render("listings/new.ejs");
 });
 
@@ -41,7 +29,6 @@ router.get(
       req.flash("error", "Listing not found!");
       return res.redirect("/listings");
     }
-    console.log(listing);
     res.render("listings/show.ejs", { listing });
   }),
 );
@@ -53,7 +40,6 @@ router.post(
   validateListing,
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
-    console.log(req.user);
     newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success", "New listing created!");
@@ -65,6 +51,7 @@ router.post(
 router.get(
   "/:id/edit",
   isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res, next) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
@@ -79,15 +66,13 @@ router.get(
 //Update route
 router.put(
   "/:id",
+  isLoggedIn,
+  isOwner,
+  validateListing,
   wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-      throw new ExpressError(400, "Send valid data for listing");
-    }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-
     req.flash("success", "Listing updated successfully!");
-
     res.redirect(`/listings/${id}`);
   }),
 );
@@ -96,10 +81,10 @@ router.put(
 router.delete(
   "/:id",
   isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
     req.flash("success", "Listing deleted successfully!");
     res.redirect("/listings");
   }),
--- routes/review.js
+++ routes/review.js
@@ -2,23 +2,12 @@ const express = require("express");
 const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 const { route } = require("./listing.js");
+const {validateReview} = require("../middleware.js");
 
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
-
-//reviews
-//Post Route
+//Post review Route
 router.post(
   "/",
   validateReview,
