40e8897a0b72bfa617d2da04b0ff10e986c0cee1
Deo-Ashish <deoashish942@gmail.com>
2026-06-11 23:11:14 +0530

    changes in db_url

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/StayNest";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/StayNest";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -32,7 +33,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
